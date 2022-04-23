e3524f0774cdc4d4316016d37a1d2a0880d55dbe
Luca Barone <baro.luc@gmail.com>
2022-04-23 21:10:31 +0200

    ✨ Add MyPick type

---
+++ src/MyPick.test.ts
@@ -0,0 +1,22 @@
+import {Equal} from './Equal'
+import {ExpectTrue} from './Expect'
+import {MyPick} from './MyPick'
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+export type cases = [
+  ExpectTrue<Equal<{title: string}, MyPick<Todo, 'title'>>>,
+  ExpectTrue<
+    Equal<
+      {title: string; completed: boolean},
+      MyPick<Todo, 'title' | 'completed'>
+    >
+  >,
+
+  // @ts-expect-error
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+]
